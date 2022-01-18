8187f8b8f9770cf7379c638ea929fa9a3c28dd8e
Quinn Zona <qzona@bsd220.org>
2022-01-18 10:19:37 -0600

    Initial Commit

---
+++ Assignment notebook/Assignment_notebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_notebookApp.swift
+//  Assignment notebook
+//
+//  Created by Student on 1/18/22.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_notebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
